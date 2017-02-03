4943311c30d71b353f88924b988d11493edb75f0
Sibusten <Sibusten@gmail.com>
2017-02-03 14:59:52 -0500

    Create pastebin-greentext.user.js

---
+++ pastebin-greentext.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        Pastebin Greentext
+// @namespace   https://github.com/Sibusten
+// @include     http://pastebin.com/*
+// @require     https://code.jquery.com/jquery-3.1.1.min.js
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+$(document).ready(function(){
+  $doc = $("#selectable > ol").first();
+  if($doc.hasClass("text"))
+  {
+    $(".de1, .de2").each(function()
+    {
+      var text = $(this).text();
+      var regex = /^>/;
+      if(regex.test(text))
+      {
+        $(this).css('color', '#789922');
+      }
+    });
+  }
+});
